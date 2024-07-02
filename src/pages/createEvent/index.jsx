6ca01045500cfdab6c508d39ca6f2cbb7de7d5e6
ckeziahp <ckeziah95@gmail.com>
2024-07-02 09:29:05 +0000

    Changes to create events page

--- src/pages/createEvent/index.jsx
+++ src/pages/createEvent/index.jsx
@@ -1,8 +1,17 @@
 const CreateEvent = () => {
   return (
     <div>
+      <div>
+        <h1>Create New Event</h1>
+        <p>
+          Event Name
+        </p>
+      </div>
+<input placeholder="text"/>
+      <div>
+        <button>March 2024</button>
+      </div>
 
-      <h1>Create New Event</h1>
     </div>
 
   )
